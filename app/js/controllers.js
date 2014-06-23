0186a9d1e97e378ae04312c00c4c098e0882b700
yougang <gangyou@gmail.com>
2014-06-23 13:19:33 +0800

    样式设计, bootstarp继承

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,8 +3,19 @@
 /* Controllers */
 
 angular.module('myApp.controllers', [])
-  .controller('MyCtrl1', ['$scope', function($scope) {
+  .controller('ListCtrl', ['$scope', function($scope) {
+    var _getWeekOfYear = function(){
+        var today = new Date();
+        var daysPassed = (today - new Date(today.getFullYear(), 0, 1)) / ( 24 * 3600 * 1000 ) + 1;
+        return Math.ceil(daysPassed / 7) + 1;
+    };
 
+    $scope.init = function(){
+        var currentWeek = _getWeekOfYear() - 1;
+        $('.week_item:eq(' + currentWeek + ')').addClass('current');
+        $('.week_item:lt(' + currentWeek + ')').addClass('passed');
+
+    }
   }])
   .controller('MyCtrl2', ['$scope', function($scope) {
 
