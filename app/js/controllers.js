a669093b81ac70fa981904acc4f84d7aef910104
yougang <gangyou@gmail.com>
2014-06-23 16:03:52 +0800

    第一版本测试

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,18 +3,26 @@
 /* Controllers */
 
 angular.module('myApp.controllers', [])
-  .controller('ListCtrl', ['$scope', function($scope) {
+  .controller('ListCtrl', ['$scope', 'Event',function($scope, Event) {
     var _getWeekOfYear = function(){
         var today = new Date();
         var daysPassed = (today - new Date(today.getFullYear(), 0, 1)) / ( 24 * 3600 * 1000 ) + 1;
-        return Math.ceil(daysPassed / 7) + 1;
+        return Math.ceil(daysPassed / 7);
     };
 
     $scope.init = function(){
-        var currentWeek = _getWeekOfYear() - 1;
+        var currentWeek = _getWeekOfYear();
         $('.week_item:eq(' + currentWeek + ')').addClass('current');
         $('.week_item:lt(' + currentWeek + ')').addClass('passed');
 
+        var events = Event.query(function(){
+            angular.forEach(events, function(value, key){
+                var week = value.week;
+                var nameNode = $('<span/>').text(value.name).addClass('name');
+                var itemNode = $('<span/>').text(value.item).addClass('item');
+                $('.week_item:eq(' + week + ')').addClass('comming').empty().append(nameNode).append(itemNode);
+            })
+        });
     }
   }])
   .controller('MyCtrl2', ['$scope', function($scope) {
--- app/js/services.js
+++ app/js/services.js
@@ -5,5 +5,7 @@
 
 // Demonstrate how to register services
 // In this case it is a simple value service.
-angular.module('myApp.services', []).
-  value('version', '0.1');
+angular.module('myApp.services', ['ngResource'])
+    .factory('Event', function($resource){
+        return $resource('http://localhost:8080/events/:eventId', {}, {})
+    })
